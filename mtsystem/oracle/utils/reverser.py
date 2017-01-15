c6ed0ebc97b2ad8fcb0ac391ae6b2c07a0466778
jbuckman <jacobbuc@andrew.cmu.edu>
2017-01-14 22:11:06 -0500

    cleanup directories

--- mtsystem/oracle/utils/reverser.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-for line in sys.stdin:
-	halves = line.strip().split('|||')
-	print halves[1].lower() + ' ||| ' + halves[0].lower()
--- nt-parser/tree-xform/README.md
+++
@@ -1,4 +0,0 @@
-To convert trees:
-
-    ./tree-xform.pl --no-explicit_terminal_reduce < test.tree
-
